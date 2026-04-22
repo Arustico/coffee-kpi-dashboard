6518e85b81bb768db42d0188086b6de7305a5aa1
Ariel <ariel.nunez.sa@gmail.com>
2026-04-22 17:44:31 -0400

    Incorporación de gitignore

---
+++ shared/database.py
@@ -0,0 +1,9 @@
+import sqlite3
+
+DB_PATH = "coffee.db"
+
+def get_connection():
+    conn = sqlite3.connect(DB_PATH)
+    conn.row_factory = sqlite3.Row
+    conn.execute("PRAGMA foreign_keys = ON;")
+    return conn
