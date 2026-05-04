2b6d789d01ad9809fd567f23e705a0b8faba4433
Ariel <ariel.nunez.sa@gmail.com>
2026-05-04 01:53:18 -0400

    Se agregan validaciones para ventas

--- modulos/ventas/create_sale/schemas.py
+++ modulos/ventas/create_sale/schemas.py
@@ -1,15 +1,17 @@
 from pydantic import BaseModel, Field
-from typing import List
+from typing import List, Optional
 
 """
 Definición de cómo llega la venta. Valida los datos de lo contrario arroja un error.
+  Contrains: Ventas mayor a 0 y menores o iguales a 99
 """
 
 class SaleItemCreate(BaseModel):
-    product_id: int
-    quantity: int
+  product_id: int
+  quantity: int = Field(gt=0, le=99)
 
 class SaleCreate(BaseModel):
-    employee_id: int
-    turn_id: int
-    items: List[SaleItemCreate]
+  employee_id: int = Field(gt=0, le=9)
+  turn_id: Optional[int] = None
+  items: List[SaleItemCreate]
+
