a241e23205c58071143cc29b11fc7b629ad707e7
Ariel <ariel.nunez.sa@gmail.com>
2026-05-04 01:54:32 -0400

    Optimizan consultas (repository.py) e incluyen manejo de errores con HTTPException (service.py)

--- modulos/ventas/create_sale/repository.py
+++ modulos/ventas/create_sale/repository.py
@@ -3,12 +3,75 @@ Definición de funciones para insertar en la base de datos.
   - Solo ejecuta SQL por lo que recibirá la conn (conexión fastAPI)
 
 """
+#-----------------------------------
+# Librerias
+#-----------------------------------
+
+from fastapi import HTTPException
+
+from datetime import datetime
 
 
+#-----------------------------------
+# Funcion para validación existencia de empleado
+#-----------------------------------
+
+def get_current_turn(conn):
+  """
+  Obtiene el turno automáticamente a partir de la hora de conexión.
+  """
+  # obtención del momento de la conexión
+  current_time = datetime.now().strftime("%H:%M:%S")
+  row = conn.execute("""
+    SELECT
+        id,
+        label,
+        active
+    FROM Turn
+    WHERE active = 1
+    AND start_time <= ?
+    AND end_time >= ?
+  """, (
+    current_time,
+    current_time
+  )).fetchone()
+
+  return row
+
+def get_turn_by_id(conn, turn_id):
+  """
+  Obtiene el turno acorde al turno_id ingresado manualmente
+  """
+  row = conn.execute("""
+    SELECT
+      id,
+      label,
+      active
+    FROM Turn
+    WHERE id = ?
+  """, (turn_id,)).fetchone()
+  return row
+
+#-----------------------------------
+# Funcion identificación del turno
+#-----------------------------------
+def employee_is_active(conn, employee_id):
+  row = conn.execute("""
+    SELECT active
+    FROM Employee
+    WHERE id = ?
+  """, (employee_id,)).fetchone()
+
+  if row is None:
+    return None
+
+  return bool(row["active"])
+
+#-----------------------------------
 # Inserción de una venta
-def insert_sale(conn, employee_id, turn_id, total_amount):
-  conn = get_connection()
+#-----------------------------------
 
+def insert_sale(conn, employee_id, turn_id, total_amount):
   cursor = conn.execute(
     """
     INSERT INTO Sale (
@@ -24,9 +87,9 @@ def insert_sale(conn, employee_id, turn_id, total_amount):
 
   return cursor.lastrowid
 
-
+#-----------------------------------
 # Insertación de item
-
+#-----------------------------------
 def insert_sale_item(conn, sale_id, product_id, quantity, unit_price):
   conn.execute(
     """
@@ -44,9 +107,9 @@ def insert_sale_item(conn, sale_id, product_id, quantity, unit_price):
       quantity,
       unit_price))
 
-
+#-----------------------------------
 # Obtención de precio del producto
-
+#-----------------------------------
 def get_product_price(conn, product_id):
 
   row = conn.execute("""
@@ -54,6 +117,12 @@ def get_product_price(conn, product_id):
     FROM Product
     WHERE id = ?
   """, (product_id,)).fetchone()
+
+  if row is None:
+    raise HTTPException(
+      status_code=404,
+      detail=f"Producto {product_id} no existe"
+    )
 
   return row["base_price"]
 
--- modulos/ventas/create_sale/service.py
+++ modulos/ventas/create_sale/service.py
@@ -2,32 +2,92 @@
 # - service.py controla la transacción
 # - repository.py SOLO ejecuta SQL
 
+# Para el manejo de errores
+# ─────────────────────────────────────────────
+# LIBRERIAS
+# ─────────────────────────────────────────────
+from fastapi import HTTPException
+
 from shared.database import get_connection
 
 from .repository import ( # funciones para insertar en bd
+  employee_is_active,
   insert_sale,
   insert_sale_item,
-  get_product_price
+  get_product_price,
+  get_turn_by_id,
+  get_current_turn
 )
+# ─────────────────────────────────────────────
+# Funciones
+# ─────────────────────────────────────────────
 
+#-----------------------------------
+# Identificación del turno
+#-----------------------------------
+def resolve_turn(conn, data):
+  # Modo admin/manual
+  if data.turn_id is not None:
+    turn = get_turn_by_id(
+      conn,
+      data.turn_id)
+
+    if turn is None:
+      raise HTTPException(
+        status_code=404,
+        detail="No existe turno en este horario")
+
+    if not turn["active"]:
+      raise HTTPException(
+        status_code=400,
+        detail="Turno inactivo")
+  # Modo automático
+  else:
+    turn = get_current_turn(conn)
+    if turn is None:
+      raise HTTPException(
+        status_code=400,
+        detail="No existe turno activo en este horario")
+  return turn
+
+#-----------------------------------
+# creación de venta
+#-----------------------------------
 def create_sale(data):
   # Inicia conexión
   conn = get_connection()
 
   try:
     conn.execute("BEGIN") # explicitamos inicio de conexión
-    total_amount = 0
+
+    # Validación del turno
+    turno = resolve_turn(conn, data)
+    turn_id = turno["id"]
+
+    # Validación del empleado
+    employee_status = employee_is_active(conn, data.employee_id)
+
+    if employee_status is None:
+      raise HTTPException(
+        status_code=404,
+        detail="Empleado no existe")
+
+    if not employee_status:
+      raise HTTPException(
+        status_code=400,
+        detail="Empleado inactivo")
+
     # calculo total de precio
+    total_amount = 0
     for item in data.items:
       price = get_product_price(conn, item.product_id)
       total_amount += price * item.quantity
-
 
     # Creación de venta
     sale_id = insert_sale(
       conn,
       data.employee_id,
-      data.turn_id,
+      turn_id,
       total_amount
     )
 
@@ -47,10 +107,19 @@ def create_sale(data):
     return {
       "sale_id": sale_id,
       "total_amount": total_amount}
-  except:Exception as e:
-    # deshacer TODO
+  except HTTPException:
     conn.rollback()
-    raise e
+    raise
+
+  except Exception as e:
+    # deshacer todo
+    conn.rollback()
+    raise HTTPException(
+      status_code=500,
+      detail=f"Error interno al crear venta {e}"
+    )
+
   finally:
     conn.close()
+
 
